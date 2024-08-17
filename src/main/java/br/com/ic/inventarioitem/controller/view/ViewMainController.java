45bc1b7ef5fc7cbbb872191e6fc60decd6850de3
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-08-17 16:54:10 -0300

    FEAT: Pagina index

--- src/main/java/br/com/ic/inventarioitem/controller/view/ViewMainController.java
+++ src/main/java/br/com/ic/inventarioitem/controller/view/ViewMainController.java
@@ -1,4 +1,13 @@
 package br.com.ic.inventarioitem.controller.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping
 public class ViewMainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
