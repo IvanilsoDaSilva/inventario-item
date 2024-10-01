4c9b20b5a50beac0a06bc18b5971f02770a3b33c
Ivanilso da Silva Conceicão Filho <66145206+IvanilsoDaSilva@users.noreply.github.com>
2024-09-30 21:50:57 -0300

    Update ItemController.java

--- src/main/java/br/com/ic/inventarioitem/controllers/ItemController.java
+++ src/main/java/br/com/ic/inventarioitem/controllers/ItemController.java
@@ -16,18 +16,6 @@ public class ItemController {
     @Autowired
     ItemService itemService;
     
-    @Operation(summary = "Cria um item")
-    @ApiResponses(value={ 
-      @ApiResponse(
-    		  responseCode = "201",
-              description = "Item criado",
-    		  content = { @Content(mediaType = "application/json",
-    		  schema = @Schema(implementation = Item.class)) }),
-      @ApiResponse(
-    		  responseCode = "400",
-    		  description = "Informações inválidas",
-    		  content = @Content)
-    })
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public Item create(
@@ -37,22 +25,6 @@ public class ItemController {
         return itemService.create(body);
     }
 
-    @Operation(summary = "Localiza um item pelo seu nome")
-    @ApiResponses(value={ 
-      @ApiResponse(
-    		  responseCode = "201",
-    		  description = "Item encontrado",
-    		  content = { @Content(mediaType = "application/json",
-    		  schema = @Schema(implementation = Item.class)) }),
-      @ApiResponse(
-    		  responseCode = "400",
-    		  description = "Informações invalidas",
-    		  content = @Content),
-      @ApiResponse(
-    		  responseCode = "404",
-    		  description = "Item não encontrado",
-    		  content = @Content)
-    })
     @PostMapping("/read-by-name/{name}")
     @ResponseStatus(HttpStatus.OK)
     public Item readByName(
@@ -63,17 +35,6 @@ public class ItemController {
         return itemService.readByNome(body);
     }
 
-    @Operation(summary = "Localiza todos itens")
-    @ApiResponses(value={ 
-      @ApiResponse(
-    		  responseCode = "201",
-    		  description = "Itens encontrado", 
-    		  content = { @Content( mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Item.class)))}),
-      @ApiResponse(
-    		  responseCode = "404",
-    		  description = "Itens não encontrado", 
-    		  content = @Content)
-      })
     @GetMapping("/read-all")
     @ResponseStatus(HttpStatus.OK)
     public List<Item> readAll(
@@ -82,36 +43,12 @@ public class ItemController {
         return itemService.readAll();
     }
 
-    @Operation(summary = "Atualiza um item")
-    @ApiResponses(value={ 
-      @ApiResponse(
-    		  responseCode = "201",
-    		  description = "Itens atualizado", 
-    		  content = { @Content(mediaType = "application/json", 
-    		  schema = @Schema(implementation = Item.class)) }),
-      @ApiResponse(
-    		  responseCode = "400",
-    		  description = "Informações inválidas", 
-    		  content = @Content)
-      })
     @PostMapping("/update")
     @ResponseStatus(HttpStatus.OK)
     public String update() {
         return null;
     }
 
-    @Operation(summary = "Deleta um item pelo seu id")
-    @ApiResponses(value={ 
-      @ApiResponse(
-    		  responseCode = "204",
-    		  description = "Item deletado",
-    		  content = { @Content(mediaType = "application/json",
-    		  schema = @Schema(implementation = Item.class)) }),
-      @ApiResponse(
-    		  responseCode = "404",
-    		  description = "Item não encontrado",
-    		  content = @Content)
-    })
     @PostMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public String delete() {
